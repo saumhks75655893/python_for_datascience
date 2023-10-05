24ce0b2b53c99b605acd65b781a958850cd97f04
himanshu <himanshukumar75655893@gmail.com>
2023-10-05 13:25:10 +0530

    third commit

--- 01_basic.py
+++ 01_basic.py
@@ -1,11 +1,31 @@
 print("Hello world")
 print("This is my first program in python.")
+
+
+#  variable - Variable is a like a bucket which store some this, just like that variables also stores the different type of values.
+
+# operators -  Operators are the symbols which works on operands. it can be unary , binary or turnarry.
+
 a = 10
 b = 50
 c = a + b
 print(c)
+print(type(c))
 
+x = 5.7
+print(type(x))
 
-#  variable - Variable is a like a bucket which store some this, just like that variables also stores the different type of values. 
+x = "abc"
+print(type(x))
 
-# operators -  Operators are the symbols which works on operands. it can be unary , binary or turnarry. 
+a, b, c = 10, 10.2, 'a'
+print(type(a))
+print(type(b))
+print(type(c))
+
+'''
+del a   # it will delete this a variable 
+print(a)
+
+'''
+
