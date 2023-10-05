93c12838049521429b4e44dfe33a71a812c44fe6
himanshu <himanshukumar75655893@gmail.com>
2023-10-05 13:17:17 +0530

    modified

--- 01_basic.py
+++ 01_basic.py
@@ -1,1 +1,11 @@
 print("Hello world")
+print("This is my first program in python.")
+a = 10
+b = 50
+c = a + b
+print(c)
+
+
+#  variable - Variable is a like a bucket which store some this, just like that variables also stores the different type of values. 
+
+# operators -  Operators are the symbols which works on operands. it can be unary , binary or turnarry. 
