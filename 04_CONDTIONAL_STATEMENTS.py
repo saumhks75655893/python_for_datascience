7a758ef0daa16df3aa1d95a19302f14d6f621ca2
himanshu <himanshukumar75655893@gmail.com>
2023-10-06 16:07:02 +0530

    conditional statements are modified

--- 04_CONDTIONAL_STATEMENTS.py
+++ 04_CONDTIONAL_STATEMENTS.py
@@ -1,5 +1,5 @@
 #  CONDITIONAL STATEMENT : ----------
-
+'''
 a = int(input("Enter the value of a : "))
 b = int(input("Enter the value of b : "))
 
@@ -21,27 +21,44 @@ if (a > b):
     print("I am still inside the if block.")
 else:
     print("I am out of if condition.")
-
+'''
 # IF - ELIF - ELSE STATEMENT : --------------
-
+'''
 if (b > a):
     print(f"b={b} is greater than a={a}.")
 elif (a == b):
     print(f"a={a} and b={b} is equal.")
 else:
     print(f"a={a} is greater than b={b}.")
-
+'''
 # i. short hand if : ---------
 
-print("A") if a > b else print("=") if a == b else print("B")
+# print("A") if a > b else print("=") if a == b else print("B")
 
-# NESTED - IF : ---------------------
+# NESTED - If - else : ---------------------
 
 x = int(input("Enter a number  : "))
 if x > 10:
-    print("Your Number is above ten, ")
-    x = int(input("Enter a number  : "))
+    print(f"Your Number is above ten = {x}")
     if x > 20:
-        print("and also above 20 !.")
+        print(f"and also above 20 !. = {x}")
+        if x > 30:
+            print(f" and really , it's also greater than 30. = {x} ")
+        else:
+            print(f"But not greater than 30. = {x}")
     else:
-        print("but not above than 20 !. ")
+        print(f"but not above than 20 !. = {x} ")
+
+else:
+    print("Small than 10. ")
+    if x < 5:
+        print(f"And also smaller than 5. = {x}")
+        if x < 0:
+            print(f"This is a negative number. {x}.")
+        else:
+            print(f"It's not a negative number  =  {x}")
+    else:
+        print(f"But not smaller than 5. {x}")
+
+
+print("Outside all if else statement.")
