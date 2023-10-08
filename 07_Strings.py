fa887b490dac31aa5ce040c7439b10c6dcdf806a
himanshu <himanshukumar75655893@gmail.com>
2023-10-08 15:28:14 +0530

    Strings.py modified

--- 07_Strings.py
+++ 07_Strings.py
@@ -25,10 +25,22 @@ print(f'%#x'%65)
 
 ''' 0 - causes padding with '0' in numbers'''
 print(f"%05d"%221)
+print(f"%05d"%21)
+print(f"%05d"%1)
 
 ''' The - tag - When a formatted value is shorter than the speicfied field width, it is usually right-justified.'''
 
+print(f"%-5d"%221)
+print(f"%-5d"%21)
+print(f"%-5d"%1)
+
 ''' The + flag and ' '  -  By default numeric value do not have a leading sign character. The + flag add '+' character to the left of numeric output. '''
 
+print(f"%+d"%221)
+print(f"%+d"%21)
+print(f"%+d"%-1)
+print("\n")
+print(f"% d"%221)
+print(f"% g"%21)
 
 
