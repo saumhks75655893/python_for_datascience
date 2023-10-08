9b2c948362bdcb15ef26ec7faa6132977cdd5a6b
himanshu <himanshukumar75655893@gmail.com>
2023-10-08 16:09:55 +0530

    files modified

--- 07_Strings.py
+++ 07_Strings.py
@@ -1,10 +1,18 @@
 # String Conversion types : ---------
 
-print(f"a = %s"%"Hello") # this will print hello without single inverted comma
-print(f"b = %r"%"Hello") # this will print hello with single inverted comma.
-print(f"c = %a"%'Hello') # this will also print hello with single inverted comma.
+# this will print hello without single inverted comma
+print(f"a = %s" % "Hello")
+print(f"b = %r" % "Hello")  # this will print hello with single inverted comma.
+# this will also print hello with single inverted comma.
+print(f"c = %a" % 'Hello')
 
 # Widht specifier : -----------
 
-print(f"a = %15s"%'Himanshu kumar')
+print(f"a = %15s" % 'Himanshu kumar')
 
+
+a = "Himanshu kumar"
+b = " is a student"
+c = a + b
+
+print(f'Sum of {a!s:10} and {b!a:10} is : \n{c!r:10}')
