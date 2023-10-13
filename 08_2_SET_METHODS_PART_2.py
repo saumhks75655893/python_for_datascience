6a4ca2ee19031e8d67930da8f5bdc7c961ed5365
himanshu <himanshukumar75655893@gmail.com>
2023-10-13 16:18:58 +0530

    08_2_SET_METHODS_PART_2.py modified and two new file 09_0_FROZEN_SET.py and 10_TUPLE.py ADDED

--- 08_2_SET_METHODS_PART_2.py
+++ 08_2_SET_METHODS_PART_2.py
@@ -56,8 +56,8 @@
 # print(a)
 # print(b)
 
-# ''' 
-# intersection() - The intersection() method returns a new set with elements that are common to all sets. It doesn't modify original sets. 
+# '''
+# intersection() - The intersection() method returns a new set with elements that are common to all sets. It doesn't modify original sets.
 # '''
 
 # s1 = {1,2,3,4}
@@ -67,8 +67,8 @@
 # print(s1)
 # print(s2)
 
-# ''' 
-# intersection_update()  -  The intersection_update() method find common elements in all set. It does modify original sets. 
+# '''
+# intersection_update()  -  The intersection_update() method find common elements in all set. It does modify original sets.
 # '''
 
 # s1 = {1,2,3,4}
@@ -80,7 +80,52 @@
 # print(s1)
 # print(s2)
 
-'''pop() - the pop() method removes element from anywhere in a set. '''
-s1 = {1,2,3,4}
-r = s1.pop()
+# '''pop() - the pop() method removes element from anywhere in a set. '''
+# s1 = {1,2,3,4}
+# r = s1.pop()
+# print(r)
+
+# '''issubset() - It returns True when all the elements of a set is present in another set b than the a set will be the subset of b set. Otherwise it will return False.
+#  But b set will be the superset of set a.
+#   issuperset() is the method to check superset condition. '''
+
+# a = {1,2,3,4}
+# b = {1,2,3,4,5,6,7}
+# r = a.issubset(b)
+# r1 = b.issuperset(a)
+# print(r)
+# print(r1)
+
+# ''' isdisjoint() - both sets have no commong element then it will be called disjoint sets. '''
+
+# s1 = {1,2,3,4}
+# s2 = {5,6,7,8}
+# r = s1.isdisjoint(s2)
+# print(r)
+
+
+# BUILT - IN FUNCTION WITH SET : ---------
+
+# s = {1, 8, 645, 23, 0, 4, 34, 76, 34, 1, 2, 35, 453, 5463, 34532, 6452}
+# r = all(s)
+# print(r)
+# r = any(s)
+# print(r)
+
+s = {1, 8, 645, 23, 0, 4, 34, 76, 34, 1, 2, 35, 453, 5463, 34532, 6452}
+r = enumerate(s)
+print(r)
+
+# s = {'', 1, 2, False, True, False, False, False}
+# r = len(s)
+# print(r)
+
+s = {1, 8, 645, 23, 0, 4, 34, 76, 34, 1, 2, 35, 453, 5463, 34532, 6452}
+r = max(s)
+print(r)
+r = min(s)
+print(r)
+r = sorted(s)
+print(r)
+r = sum(s)
 print(r)
