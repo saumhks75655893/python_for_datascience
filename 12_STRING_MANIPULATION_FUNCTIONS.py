25b0c42daf6c5c1e459c1da58bd9cfc8616b6580
himanshu <himanshukumar75655893@gmail.com>
2023-10-17 23:06:06 +0530

    file added

--- 12_STRING_MANIPULATION_FUNCTIONS.py
+++ 12_STRING_MANIPULATION_FUNCTIONS.py
@@ -1,2 +1,51 @@
 # ------------------ : STRING MANIPULATION FUNCTION : ---------------------------- #
+# 1.
+# S = 'a@python'
+# print(S.isidentifier())
 
+#  2.
+# a = '\n'
+# print(a.isprintable())
+
+# 3.
+# a = 'ram is a good boy'
+# a = ''
+# r = a.isspace()
+# print(r)
+# a = '\r'
+# r = a.isspace()
+# print(r)
+# a = '\b'
+# r = a.isspace()
+# print(r)
+# a = '\n'
+# r = a.isspace()
+# print(r)
+# a = '\a'
+# r = a.isspace()
+# print(r)
+# a = '\t'
+# r = a.isspace()
+# print(r)
+
+# 4.
+
+# a = "Ram Is A Good Boy"
+# r = a.istitle()
+# print(r)
+
+# 5.
+# s = '   This is Python   '
+# print(s)
+# r = s.strip(' Tn ')
+
+# s = '   This is Python   '
+# print(r)
+# r = s.rstrip(' Tn ')
+
+# s = '   This is Python   '
+# print(r)
+# r = s.lstrip(' Tn ')
+# print(r)
+
+# 6. 
